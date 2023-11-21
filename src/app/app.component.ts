c97e52ed8be4f8859c5ae8c44df1830bc478b8b7
noobcoder24 <zainnaeemk10@gmail.com>
2023-11-21 22:30:49 +0500

    Basic header changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  shouldDisplaySidebar: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.shouldDisplaySidebar = this.shouldShowSidebar(event.url);
+      }
+    });
+    
+  }
+  private shouldShowSidebar(url: string): boolean {
+    // Implement your logic to determine if the sidebar should be displayed
+    // For example, you can check the URL or any other conditions
+    return url !== '/login' && url!=='/';
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-
 export class HeaderComponent {
-  constructor(private router: Router) { }
-
-  myClick(){
-    this.router.navigate(['/secondComponent'])
+  constructor(private router: Router) {}
+  @Input() title: string = '';
+  @Input() semiTitle: string = '';
+  myClick() {
+    this.router.navigate(['/secondComponent']);
   }
 }
